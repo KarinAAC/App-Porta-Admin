7c83e789a8ee7673a838d8429caf4fd7ba105b8a
KarinAAC <karin_alejo@hotmail.com>
2017-08-02 18:22:59 -0500

    subiendo pagina Postear Articulos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   $('#Registrar').click(function(){
     window.location.href = "index.html";
-  });  
+  });
+  
+    $("#input-4").fileinput({showCaption: false});
 });
