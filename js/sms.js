eed9ef5425a4785ba6f64e746a068519d57af24d
AnaLucia20 <analuciarodriguez.a@gmail.com>
2017-08-03 16:12:26 -0500

    dashboard

--- js/sms.js
+++ js/sms.js
@@ -13,7 +13,7 @@ client.messages.create({
     body: "hola"
 }, function(err) {
     console.log(err);
-});*/
+});
 
 // Download the Node helper library from twilio.com/docs/node/install
 // These vars are your accountSid and authToken from twilio.com/user/account
@@ -28,4 +28,25 @@ client.calls.create({
     from: "+5117097669"
 }, function(err, call) {
     console.log(call.sid);
+});
+*/
+
+
+//Initialize a REST client in a single line:
+var client = require('twilio')('TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN');
+ 
+// Use this convenient shorthand to send an SMS:
+client.sendSms({
+    to:'YOUR_PHONE',
+    from:'TWILIO_NUMBER',
+    body:'ahoy hoy! Testing Twilio and node.js'
+}, function(error, message) {
+    if (!error) {
+        console.log('Success! The SID for this SMS message is:');
+        console.log(message.sid);
+        console.log('Message sent on:');
+        console.log(message.dateCreated);
+    } else {
+        console.log('Oops! There was an error.');
+    }
 });
